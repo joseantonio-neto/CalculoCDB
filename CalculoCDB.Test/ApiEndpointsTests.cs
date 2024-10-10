4c6540b7b1602d3082ff7a941d5910a1e72bc66e
José Antonio dos Santos Neto <joseantonio.neto@outlook.com>
2024-10-09 23:34:33 -0300

    Testes das WebApi .NET

---
+++ CalculoCDB.Test/ApiEndpointsTests.cs
@@ -0,0 +1,48 @@
+﻿using CalculoCDB.Server;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Diagnostics.CodeAnalysis;
+
+namespace CalculoCDB.Test
+{
+	[ExcludeFromCodeCoverage]
+	public class ApiEndpointsTests : IClassFixture<WebApplicationFactory<Program>>
+	{
+		private readonly WebApplicationFactory<Program> _factory;
+
+		public ApiEndpointsTests(WebApplicationFactory<Program> factory)
+		{
+			_factory = factory;
+		}
+
+		[Theory]
+		[InlineData("/calculate?initialValue=1000.0&months=10")]
+		public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+		{
+			// Arrange
+			var client = _factory.CreateClient();
+
+			// Act
+			var response = await client.GetAsync(url);
+
+			// Assert
+			response.EnsureSuccessStatusCode(); // Status Code 200-299
+			Assert.Equal("application/json; charset=utf-8", response.Content?.Headers?.ContentType?.ToString());
+		}
+
+		[Theory]
+		[InlineData("/calculate?initialValue=teste&months=teste")]
+		[InlineData("/calculate?initialValue=0&months=10")]
+		[InlineData("/calculate?initialValue=1000&months=-100")]
+		public async Task Get_EndpointsReturnBadRequest(string url)
+		{
+			// Arrange
+			var client = _factory.CreateClient();
+
+			// Act
+			var response = await client.GetAsync(url);
+
+			// Assert
+			Assert.Equal(System.Net.HttpStatusCode.BadRequest, response.StatusCode); // Status Code 400
+		}
+	}
+}
---
+++ CalculoCDB.Test/CalculateServiceTest.cs
@@ -0,0 +1,59 @@
+﻿using CalculoCDB.Server.Services;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace CalculoCDB.Test
+{
+	[ExcludeFromCodeCoverage]
+	public class CalculateServiceTest
+	{
+		private readonly CalculateService service;
+		public CalculateServiceTest()
+		{
+			service = new CalculateService();
+		}
+
+		[Theory]
+		[InlineData([950, 5, 997.07631141717832, 986.48414134831319, 0.225])]
+		[InlineData([1000, 10, 1101.563624143253, 1081.2508993146025, 0.2])]
+		[InlineData([1700, 14, 1946.5355604144136, 1903.3918373418912, 0.175])]
+		[InlineData([2500, 26, 3214.8812281098294, 3107.6490438933552, 0.15])]
+		public void GetCalculate_ReturnValue_NetIncome_GrossIncome_Fee(double initialValue, int months, double grossIncome, double netIncome, double fee)
+		{
+			// Act
+			var calculateResponse = service.Calculate(initialValue, months);
+
+			// Assert
+			Assert.NotNull(calculateResponse);
+			Assert.Equal(grossIncome, calculateResponse.GrossIncome);
+			Assert.Equal(netIncome, calculateResponse.NetIncome);
+			Assert.Equal(fee, calculateResponse.Fee);
+		}
+
+		[Fact]
+		public void GetCalculate_ThrowsExceptionForZeroValue()
+		{
+			// Act
+			Action act = () =>
+			{
+				service.Calculate(1000, 0);
+			};
+
+			// Assert
+			Assert.Throws<ArgumentException>(act);
+		}
+
+		[Fact]
+		public void GetCalculate_ThrowsExceptionForNegativeValue()
+		{
+			// Act
+			Action act = () =>
+			{
+				service.Calculate(-100, 10);
+			};
+
+			// Assert
+			Assert.Throws<ArgumentException>(act);
+		}
+	}
+}
